9a7df8bab1840ead9ca93d97ecd475547e5abce5
PanHongRainbow <panhongrainbow@gmail.com>
2025-06-17 16:45:59 +0800

    ⭐ feat ➡️ [utilhub] Add configuration file support.
    🔴 Implemented a utility that automatically generates configuration files based on Go structs.
    🔴 This tool supports setting default values, making it easier to bootstrap new projects or manage config schemas.

--- utilhub/defaultConfig_type.go
+++ utilhub/defaultConfig_type.go
@@ -1,7 +1,25 @@
 package utilhub
 
+// =====================================================================================================================
+//                  🛠️ Default Config Type (Tool)
+// Default Config Type contains types for DefaultConfig, bptreeUnitTestConfig etc. (这里收集了 DefaultConfig 等类型)
+// =====================================================================================================================
+
 // DefaultConfig ⛏️ is a type constraint that allows struct types to store default configuration values. (预设配置)
 type DefaultConfig interface{}
+
+// BptreeUnitTestConfig ⛏️ is a struct for BpTree unit test configuration.
+type BptreeUnitTestConfig struct {
+	RandomTotalCount             int64 `json:"randomTotalCount" default:"7500000"`        // 🧪 randomTotalCount represents the number of elements to be generated for random testing.
+	RandomMin                    int64 `json:"randomMin" default:"10"`                    // 🧪 randomMin represents the minimum value for generating random numbers.
+	RandomHitCollisionPercentage int64 `json:"randomHitCollisionPercentage" default:"70"` // 🧪 random number hit collision percentage.
+	// Calculate the maximum random value.
+	// randomTotalCount/randomHitCollisionPercentage*100 + randomMin = randomMax
+	// 7500000 / 70 * 100 + 10 = 10714295
+	RandomMax int64 `json:"randomMax" default:"10714295"` // 🧪 randomMax represents the maximum value for generating random numbers.
+}
+
+// types for testing is as bellows: (以下是测试用的类型) ===== ===== ===== ===== ===== ===== ===== ===== =====
 
 // testConfig ⛏️ is a test struct for DefaultConfig. (测试用的预设配置)
 type testConfig struct {
@@ -16,12 +34,4 @@ type testConfig struct {
 		PoolSize int    `json:"pool_size" default:"10"`
 	} `json:"database"`
 	Features []string `json:"features" default:"feature1,feature2,feature3"`
-}
-
-// bptreeUnitTestConfig ⛏️ is a struct for BpTree unit test configuration.
-type bptreeUnitTestConfig struct {
-	RandomTotalCount             int64 `json:"randomTotalCount" default:"7500000"`                                                            // 🧪 randomTotalCount represents the number of elements to be generated for random testing.
-	RandomMin                    int64 `json:"randomMin" default:"10"`                                                                        // 🧪 randomMin represents the minimum value for generating random numbers.
-	RandomHitCollisionPercentage int64 `json:"randomHitCollisionPercentage" default:"70"`                                                     // 🧪 random number hit collision percentage.
-	RandomMax                    int64 `json:"randomMax" default:"randomMax = randomTotalCount/randomHitCollisionPercentage*100 + randomMin"` // 🧪 randomMax represents the maximum value for generating random numbers.
 }
