047708a61c3c60d560a55e3b80f710806dd4d1fe
PanHongRainbow <panhongrainbow@gmail.com>
2026-01-07 12:35:36 +0800

    ⚗️ test ➡️ [manual] Load config values and store for reproduction.
    🔴 Add method to load configuration values and store all failure scenarios in a slice for reproduction.

---
+++ utilhub/manaulConfig.go
@@ -0,0 +1,89 @@
+package utilhub
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+	"reflect"
+)
+
+var (
+	// 🧪 Create a config instance for B plus tree unit testing and collect many previous failure scenarios.
+	_manualTestConfig []BptreeUnitTestConfig // This time, using slices that contain various different failure scenarios.
+	_manualParseErr   = ParseManual(&_manualTestConfig)
+)
+
+// ManualConfig is the instance, which refers to the file name under the config directory.
+type ManualConfig interface{}
+
+// ParseManual ⛏️ loads previous failure scenarios.
+func ParseManual(cfg ManualConfig) error {
+	return _parseManual(cfg)
+}
+
+// _parseManual ⛏️ loads the default configuration from struct tags and applies it to the provided struct,
+// prioritizing the specified values if available.
+func _parseManual(cfg ManualConfig) error {
+	// Get the default configuration directory.
+	projectPath, err := GetProjectDir(filepath.Join(ProjectName))
+	if err != nil {
+		return err
+	}
+
+	// Get the struct name to use as the filename.
+	file, err := GetDefaultStructName(&cfg)
+	if err != nil {
+		return err
+	}
+
+	// _applyDefaults2manualConfig will fill in the missing fields of the instance with default values.
+	err = _applyDefaults2manualConfig(filepath.Join(projectPath, "config", file+".json"), cfg)
+	if err != nil {
+		return err
+	}
+
+	// If the record is configured to be inside the project directory,
+	// prepend the project path to the test record path.
+	arr := *cfg.(*[]BptreeUnitTestConfig)
+	for i := 0; i < len(arr); i++ {
+		if arr[i].Record.IsInsideProject == true {
+			arr[i].Record.TestRecordPath = filepath.Join(projectPath, arr[i].Record.TestRecordPath)
+		}
+	}
+
+	// Return nil to indicate the operation completed successfully.
+	return nil
+
+}
+
+// _applyDefaults2manualConfig fills in default values for many previous failure scenarios.
+func _applyDefaults2manualConfig(filePath string, cfg ManualConfig) error {
+	// Check if the config is a pointer to a struct.
+	if reflect.ValueOf(cfg).Kind() != reflect.Ptr {
+		return errors.New("config must be a pointer to a struct")
+	}
+
+	// Read the default configuration from the file.
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal the JSON data into the provided config and overwrite the default values.
+	if err = json.Unmarshal(content, cfg); err != nil {
+		return err
+	}
+
+	// Extract each configuration file from the slice, and fill in the default values if any fields are missing.
+	arr := cfg.(*[]BptreeUnitTestConfig)
+	for i := 0; i < len(*arr); i++ {
+		// [applyDefaults] applies the default values from struct tags to the provided config. (主要逻辑)
+		if err = applyDefaults(&((*arr)[i])); err != nil {
+			return err
+		}
+	}
+
+	// No error occurred, return nil.
+	return nil
+}
--- utilhub/manaulConfig_test.go
+++ utilhub/manaulConfig_test.go
@@ -1,9 +1,14 @@
 package utilhub
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_ParseFailRecovery(t *testing.T) {
 	err := ParseManual(&_manualTestConfig)
+	test := _manualTestConfig
+	fmt.Println(test)
 	if err != nil {
 		t.Error("fail recovery is not nil")
 	}
